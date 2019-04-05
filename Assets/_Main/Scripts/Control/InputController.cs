c8609973b48e03200e7ea97462cc353bc0e07bd8
Danilo Vanegas <danilo.vanegas@upb.edu.co>
2019-04-05 10:03:00 -0500

    Grass Shader

---
+++ Assets/_Main/Scripts/Control/InputController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+  #region Components
+  #endregion
+
+  private void Start () {
+
+  }
+  private void Update () { }
+}
